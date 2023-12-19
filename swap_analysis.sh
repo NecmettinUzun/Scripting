c21c4a1f2d34a96e51d0d94ff94386d6ddca4196
NecmettinUzun <necmettinuzun5@gmail.com>
2023-12-19 17:44:04 +0300

    Update swap_analysis.sh

--- swap_analysis.sh
+++ swap_analysis.sh
@@ -3,15 +3,11 @@
 # Ordering all swap usage and writing them to the file 
 # -n : sort numerically
 # -r : reverse the results. Default is small to large
-# -k : 2.kolona gore siralama yap.Default dosyanin sonuna kadar ama aralikta verilebilir.
+# -k : Which column to be used for ordering
 
 grep VmSwap /proc/*/status | sort -k 2 -n -r > tmp.txt
-
-
-# en cok swap kullanan 10 processi baska dosyaya al
 head -10 tmp.txt > tmp2.txt
 
-# Dosyayi satir satir oku ve PID degerine gore adini bul
 cat tmp2.txt | while read LINE
 do
 processID=`echo $LINE | awk -F "/" '{ print $3 }'`
